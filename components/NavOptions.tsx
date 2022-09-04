e0c6e4b80d181294a13024d6d3c93e2a05f8f4a0
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-04 20:57:44 +0200

    adding some styling

--- components/NavOptions.tsx
+++ components/NavOptions.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import React from "react";
 import { data } from "../helpers/data";
+import tw from "tailwind-react-native-classnames";
 
 const NavOptions = () => {
   return (
@@ -16,12 +17,17 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity>
+        <TouchableOpacity
+          style={tw`
+        p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40
+        `}
+        >
           <View>
             <Image
               source={{ uri: item.image }}
-              style={{ width: 120, height: 120 }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
             />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
           </View>
         </TouchableOpacity>
       )}
--- helpers/data.ts
+++ helpers/data.ts
@@ -8,7 +8,8 @@ export const data = [
   {
     id: "2",
     title: "Order food",
-    image: "https://links.papareact.com/28w",
+    image:
+      "https://play-lh.googleusercontent.com/WSolXsO3HwiJSS-lXWgv1llcHHsbkdMUvhfeOz-rIsBh7_RnpdA2sLe58o3KjXY0LQ",
     screen: "EatsScreen", //To change in the future
   },
 ];
