cccfc1382bd65fc28fa6500692e85a8c1c9f2f5f
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-04 19:16:38 +0200

    create the home screen component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-
-// Set up Redux
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text></Text>
+        <HomeScreen />
       </View>
     </Provider>
   );
