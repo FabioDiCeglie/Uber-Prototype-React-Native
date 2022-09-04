ee472da16ec6f1aa56b4bbc47ae8a8b9559b915b
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-04 19:27:58 +0200

    implementing the logo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, Text, View, Image, StyleSheet } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+});
