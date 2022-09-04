59ff61278fd66aa0f7e03b74a9d50b7957daec12
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-04 19:18:23 +0200

    clening some code and using safeAreaView

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
