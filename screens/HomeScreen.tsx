4accab296e8c463110c4d90de0d0cac8b9ab5579
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-07 18:58:24 +0200

    chore: dispatch the action setOrigin

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { SafeAreaView, Text, View, Image, StyleSheet } from "react-native";
+import { SafeAreaView, View, Image, StyleSheet } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// @ts-ignore
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/NavitagionSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -27,6 +31,21 @@ const HomeScreen = () => {
               fontSize: 18,
             },
           }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details?.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          // @ts-ignore
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
           debounce={400}
           query={{
             key: GOOGLE_MAPS_APIKEY,
