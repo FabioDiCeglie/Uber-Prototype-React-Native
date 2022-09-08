e0bea71ea8eb8351868b85d8cc646277ce28a76a
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-08 18:14:44 +0200

    feat: working on navigateCard

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,11 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// @ts-ignore
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}> Good morning</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
+      <View>
+        <GooglePlacesAutocomplete
+          placeholder="Where To?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {}}
+          fetchDetails={true}
+          // @ts-ignore
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          debounce={400}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
