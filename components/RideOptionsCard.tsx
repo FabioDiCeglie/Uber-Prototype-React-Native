0adce3a6fdc8f86f5a63a9ecf2ec8df07adb0e60
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-08 22:42:46 +0200

    feat: working on RideOptionsCard

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -4,14 +4,19 @@ import {
   SafeAreaView,
   View,
   TouchableOpacity,
+  FlatList,
+  Image,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
+import { dataRide } from "../helpers/data";
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -28,6 +33,36 @@ const RideOptionsCard = () => {
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
+
+      <FlatList
+        data={dataRide}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, image }, item }) => (
+          <TouchableOpacity
+            style={tw`flex-row items-center justify-between px-10 ${
+              // @ts-ignore
+              id === selected?.id && "bg-gray-200"
+            }`}
+            // @ts-ignore
+            onPress={() => setSelected(item)}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time</Text>
+            </View>
+
+            <Text style={tw`text-xl`}>$11</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   );
 };
--- helpers/data.ts
+++ helpers/data.ts
@@ -13,3 +13,30 @@ export const data = [
     screen: "EatsScreen", //To change in the future
   },
 ];
+
+export const dataRide = [
+  {
+    id: "Uber Saver",
+    title: "Uber Saver",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-X",
+    title: "Uber X",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX",
+    title: "Uber Lux",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
