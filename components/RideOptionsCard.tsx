061e47af045f0da4661f8d6d072f89fb2ee046e1
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-08 22:23:54 +0200

    chore: create a button to navigateCard

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -8,12 +8,18 @@ import {
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
-        <TouchableOpacity>
+        <TouchableOpacity
+          // @ts-ignore
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
           <Icon
             tvParallaxProperties={undefined}
             name="chevron-left"
