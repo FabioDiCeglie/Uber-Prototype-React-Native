1c88458eebc41650212027a215983b47a976a116
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-08 22:13:02 +0200

    feat: created bar in navigateCard

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
@@ -9,6 +9,7 @@ import { useNavigation } from "@react-navigation/native";
 // @ts-ignore
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import NavFavourites from "./NavFavourites";
+import { Icon } from "react-native-elements";
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -45,6 +46,38 @@ const NavigateCard = () => {
         />
 
         <NavFavourites />
+      </View>
+
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+      >
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+          // @ts-ignore
+          onPress={() => navigation.navigate("RideOptionsCard")}
+        >
+          <Icon
+            name="car"
+            type="font-awesome"
+            color="white"
+            size={16}
+            tvParallaxProperties={undefined}
+          />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+            tvParallaxProperties={undefined}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
