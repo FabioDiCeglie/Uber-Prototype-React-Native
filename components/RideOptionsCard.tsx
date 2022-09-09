cfa0e7f7a5d104137ea33520201b0502b9e3f8e4
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-09 14:30:07 +0200

    feat: added price

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -12,10 +12,15 @@ import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { dataRide } from "../helpers/data";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/NavitagionSlice";
+
+const SURGE_CHARGE_RATE = 1.5;
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -31,13 +36,15 @@ const RideOptionsCard = () => {
             type="fontawesome"
           />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride - {travelTimeInformation?.distance.text}
+        </Text>
       </View>
 
       <FlatList
         data={dataRide}
         keyExtractor={(item) => item.id}
-        renderItem={({ item: { id, title, image }, item }) => (
+        renderItem={({ item: { id, title, image, multiplier }, item }) => (
           <TouchableOpacity
             style={[
               tw`flex-row items-center justify-between px-10 ${
@@ -59,10 +66,20 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time</Text>
+              <Text>{travelTimeInformation?.duration.text} Travel Time</Text>
             </View>
 
-            <Text style={tw`text-xl`}>$11</Text>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat("en-gb", {
+                style: "currency",
+                currency: "EUR",
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
